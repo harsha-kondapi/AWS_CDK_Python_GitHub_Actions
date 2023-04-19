024031b0902362fc5f3b3780f7654c1c3d5d1ce6
harsha-kondapi <harshakondapi11@gmail.com>
2023-04-19 17:05:12 +0530

    added aws resources sqs,s3,lambda in stack

--- aws_cdk_python_git_hub_actions/aws_cdk_python_git_hub_actions_stack.py
+++ aws_cdk_python_git_hub_actions/aws_cdk_python_git_hub_actions_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
 from constructs import Construct
 
@@ -13,7 +16,23 @@ class AwsCdkPythonGitHubActionsStack(Stack):
         # The code that defines your stack goes here
 
         # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkPythonGitHubActionsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        queue = sqs.Queue(
+             self, "AwsCdkPythonGitHubActionsQueue",
+             visibility_timeout=Duration.seconds(300),
+         )
+
+        bucket = s3.Bucket(self, "cdk-source-bucket",
+                               encryption=aws_s3.BucketEncryption.KMS,
+                               bucket_key_enabled=True
+                               )
+
+        hello_function = _lambda.Function(self, "WelcomeHandler",
+                                          runtime=_lambda.Runtime.PYTHON_3_8,
+                                          handler="welcome.handler",
+                                          #   code=_lambda.Code.from_asset(path.join(__dirname, 'lambda-api'))
+                                          code=_lambda.Code.from_asset('lambda-api')
+                                          )
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_function
+        )
